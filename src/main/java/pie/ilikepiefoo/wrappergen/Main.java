84053562cb9d81a13d956576853b82a6a704d16f
Hunter19823 <hspragg@asu.edu>
2024-02-22 15:31:27 -0700

    Added Field to main example

--- src/main/java/pie/ilikepiefoo/wrappergen/Main.java
+++ src/main/java/pie/ilikepiefoo/wrappergen/Main.java
@@ -1,5 +1,7 @@
 package pie.ilikepiefoo.wrappergen;
 
+import pie.ilikepiefoo.wrappergen.builder.ClassBuilder;
+import pie.ilikepiefoo.wrappergen.builder.FieldBuilder;
 import pie.ilikepiefoo.wrappergen.builder.ImportBuilder;
 import pie.ilikepiefoo.wrappergen.builder.MethodBuilder;
 
@@ -19,6 +21,27 @@ public class Main {
             .setAccessModifier("public")
             .addModifier("static");
         System.out.println(method.toJavaFile(0));
+
+        FieldBuilder field = new FieldBuilder()
+            .setName("MY_CONSTANT")
+            .setType("String")
+            .setAccessModifier("private")
+            .addModifiers("final", "static")
+            .addAnnotation("@SuppressWarnings(\"unchecked\")");
+        System.out.println(field.toJavaFile(0));
+
+        ClassBuilder classBuilder = new ClassBuilder()
+            .setImports(importBuilder.toJavaFile(0))
+            .setName("Main")
+            .setAccessModifier("public")
+            .addModifiers("final")
+            .addAnnotations("@SuppressWarnings(\"unchecked\")")
+            .addGenerics("T")
+            .setSuperClass("Object")
+            .addInterfaces("Runnable")
+            .addBody(field.toJavaFile(1))
+            .addBody(method.toJavaFile(1));
+        System.out.println(classBuilder.toJavaFile(0));
     }
 
 }
