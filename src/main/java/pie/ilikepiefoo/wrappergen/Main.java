f443e8dce26dc43fbf1066061ace4889ad47e76e
Hunter19823 <hspragg@asu.edu>
2024-03-02 23:57:22 -0700

    Updated main to demonstrate how the wrapper works.

--- src/main/java/pie/ilikepiefoo/wrappergen/Main.java
+++ src/main/java/pie/ilikepiefoo/wrappergen/Main.java
@@ -1,14 +1,32 @@
 package pie.ilikepiefoo.wrappergen;
 
 import pie.ilikepiefoo.wrappergen.builder.WrapperClassBuilder;
-
-import java.util.ArrayList;
+import pie.ilikepiefoo.wrappergen.example.CustomWrapper;
 
 public class Main {
     public static void main(String[] args) {
+        CustomWrapper<Double> arrayListWrapper = new CustomWrapper<>();
+        arrayListWrapper.add(5.0);
+        arrayListWrapper.add(10.0);
+        System.out.println(arrayListWrapper.get(0));
+        System.out.println(arrayListWrapper.get(1));
+        arrayListWrapper.getintHandler.setCustomHandler(
+            (index) -> {
+                var value = arrayListWrapper.getintHandler.getSuperHandler().onGet(index);
+                System.out.println("The get method was called with index " + index + " and returned " + value);
+                return value;
+            }
+        );
+        System.out.println(arrayListWrapper.get(0));
+        System.out.println(arrayListWrapper.get(1));
+
+    }
+
+    public static void generateWrapperClass(Class<?> subject) {
+
         WrapperClassBuilder builder = new WrapperClassBuilder("CustomWrapper");
         builder.setPackageName("pie.ilikepiefoo.wrappergen.example");
-        builder.addClassImplementation(ArrayList.class);
+        builder.addClassImplementation(subject);
         System.out.println(builder.toJavaFile(0));
     }
 
